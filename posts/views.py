1365fdf6c25eea0066efcfe2f67bab7d25e727f0
Ubuntu <ubuntu@ip-172-31-85-80.ec2.internal>
2019-06-12 11:29:46 +0000

    Added blogpostform.html template

--- posts/views.py
+++ posts/views.py
@@ -9,7 +9,7 @@ def get_posts(request):
     of Posts that were published prior to 'now'
     and render them to the 'blogposts.html' template
     """
-    posts = Post.objects.filter(published_date_lte=timezone.now()).order_by('-published_date')
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
     return render(request, 'blogposts.html', {'posts': posts})
     
 def post_detail(request, pk):
